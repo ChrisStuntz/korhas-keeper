c2ba9ee96e8fd352bcbfe05255c9eb6a2fcf5de5
Chris <cstuntz@fastmail.com>
2024-07-19 17:42:17 -0700

    Update things for scaffolding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,14 +17,14 @@ export default function HomePage() {
     <main className="">
       <div className="flex flex-wrap gap-4">
         {
-          mockImages.map((image) => (
+          [...mockImages, ...mockImages, ...mockImages].map((image) => (
             <div key={image.id} className="w-48">
               <img src={image.url} />
             </div>
           ))
         }
       </div>
-      Howdy
+      Meowdy, Parner
     </main>
   )
 }
